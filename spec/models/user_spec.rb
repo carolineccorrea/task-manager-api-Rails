75eda97932145ebdc71ed0aa021e60444e50bc8e
Caroline <caroline.correa@netquant.com.br>
2019-09-11 16:01:35 -0300

    changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
+#testes de código
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  let (:user) { build(:user) }
+  # teste com factory girl
+  # subject { FactoryGirl.build(:user) }
+  # it { expect(subject).to respond_to(:email) }
+  # it { expect(subject).to be_valid }
 
-  it { expect(@user).to respond_to(:email) }
+ # context 'when email is a nil' do 
+ #   before { user.email = nil }
+ #   it { expect(user).not_to be_valid}
+ # end
+
+  # teste sem factory girl
+  # it {expected.to respond_to(:email) }
+ it { is_expected.to validate_presence_of(:email) }
+ it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+ it { is_expected.to validate_confirmation_of(:password) }
+ 
 
 end
